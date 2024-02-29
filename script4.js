faaafb0de4b3c1628a8d88ba31e6900c4716c06d
Rohaan <rohaansmartyboy@gmail.com>
2024-02-29 21:49:15 +0500

    assignment 4

---
+++ script4.js
@@ -0,0 +1,30 @@
+for (let i = 1; i <= 5; i++){
+    console.log(i);    
+}
+
+
+let sum = 0;
+for (let j = 1; j <= 10; j++){
+    sum = sum + j;
+}
+console.log(sum);
+
+
+for (let k = 10; k >= 1; k--){
+    console.log(k);
+}
+
+
+for (let l = 1; l <= 10; l++){
+    console.log('5 x ' + l + ' = ' + 5 * l);
+}
+
+
+//for loop can be used to make a task repeat itself for a specific number of times
+
+
+let star = ''
+for (let m = 1; m <= 5; m++){
+    star = star + '*'
+    console.log(star);
+}
