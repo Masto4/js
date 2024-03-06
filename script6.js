bac26f31650688b431b0b823b1756c16979b00a2
Rohaan <rohaansmartyboy@gmail.com>
2024-03-06 09:49:34 +0500

    assignment6

--- script6.js
+++ script6.js
@@ -2,6 +2,7 @@ let tableData = document.querySelector('tbody');
 
 const names = ['Abbas', 'Ali', 'Shahid', 'Sajjad', 'Rohaan', 'Taimoor'];
 const age = [15, 18, 12, 20, 13, 17];
+const classes = [9, 12, 6, 14, 7, 11];
 
 
 let counter = ''
@@ -10,6 +11,7 @@ for (let i = 0; i < names.length; i++){
     <tr>
        <td>${names[i]}</td>
        <td>${age[i]}</td>
+       <td>${classes[i]}</td>
     </tr>`
 }
 tableData.innerHTML = counter;
